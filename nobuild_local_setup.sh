85524585627dba2b3e17360f1436cfb262666d82
yhyu13 <yohan680919@gmail.com>
2023-05-11 01:13:15 +0800

    Add new local build script

---
+++ nobuild_local_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+OPENAI_API_KEY=""
+
+NEXTAUTH_SECRET=$(openssl rand -base64 32)
+
+LAN_PROXY=http://192.168.1.6:8889
+LOCAL_PROXY=http://127.0.0.1:8889
+
+ENV="NODE_ENV=development\n\
+NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
+NEXTAUTH_URL=http://localhost:3000\n\
+OPENAI_API_KEY=$OPENAI_API_KEY\n\
+DATABASE_URL=file:../db/db.sqlite\n\
+HTTP_PRXY=$LAN_PROXY\n\
+HTTPS_PRXY=$LAN_PROXY\n"
+
+cd next
+printf $ENV > .env
+printf $ENV > .env.docker
+
+docker stop agentgpt
+docker rm agentgpt
+
+source .env.docker
+docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
