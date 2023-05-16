6f1a71f5732dc57ec3e9c694e6ddd6a57dd8b205
yhyu13 <yohan680919@gmail.com>
2023-05-16 22:15:14 +0800

    Add local script

---
+++ local_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+source .local_env
+
+ENV="NODE_ENV=development\n\
+NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
+NEXTAUTH_URL=http://localhost:3000\n\
+OPENAI_API_KEY=$OPENAI_API_KEY\n\
+DATABASE_URL=file:../db/db.sqlite\n"
+
+printf $ENV > .env
+
+if [ "$1" = "--docker" ]; then
+  printf $ENV > .env.docker
+  source .env.docker
+  docker build --build-arg NODE_ENV=$NODE_ENV -t agentgpt .
+  docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
+elif [ "$1" = "--docker-compose" ]; then
+  docker-compose up -d --remove-orphans
+else
+  printf $ENV > .env
+  ./prisma/useSqlite.sh
+  npm install
+  npm run dev
+fi
