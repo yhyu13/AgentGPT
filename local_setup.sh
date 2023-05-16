de23bacfb870128ed86ae4efffcae3c63dc39fa4
yhyu13 <yohan680919@gmail.com>
2023-05-16 21:09:54 +0800

    Adjust local script

--- local_setup.sh
+++ local_setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
-OPENAI_API_KEY=""
+source .local_env
 
-NEXTAUTH_SECRET=$(openssl rand -base64 32)
-
-LAN_PROXY=socks5://192.168.1.6:1089
-LOCAL_PROXY=socks5://127.0.0.1:1089
+LAN_PROXY=http://192.168.1.6:8889
+LOCAL_PROXY=http://127.0.0.1:8889
 
 ENV="NODE_ENV=development\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
@@ -14,22 +12,20 @@ NEXTAUTH_URL=http://localhost:3000\n\
 NEXT_PUBLIC_BACKEND_URL=http://localhost:3000\n\
 OPENAI_API_KEY=$OPENAI_API_KEY\n\
 DATABASE_URL=file:../db/db.sqlite\n\
-HTTP_PROXY=$LAN_PROXY\n\
-HTTPS_PROXY=$LAN_PROXY\n"
+NEXT_PUBLIC_WEB_SEARCH_ENABLED=true\n\
+SERP_API_KEY=$SERP_API_KEY\n"
+
+echo $ENV
 
 cd next
 printf $ENV > .env
 printf $ENV > .env.docker
 
+cd ..
 docker stop agentgpt
 docker rm agentgpt
 
-source .env.docker
-#NO_CACHE="--no-cache"
-NO_CACHE=""
-docker build $NO_CACHE --build-arg NODE_ENV=$NODE_ENV --build-arg LOCAL_PROXY=$LOCAL_PROXY --network host -f local_Dockerfile -t agentgpt . 
-docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
+docker-compose -f ./local-docker-compose.yml up --build -d --remove-orphans
 
-cd ..
 sleep 10
 xdg-open http://localhost:3000
