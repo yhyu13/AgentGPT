5403610cbc6d2e0aa08e969a03255406711f9f26
yhyu13 <yohan680919@gmail.com>
2023-05-16 21:09:54 +0800

    Adjust local script

--- nobuild_local_setup.sh
+++ nobuild_local_setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
-OPENAI_API_KEY=""
-
-NEXTAUTH_SECRET=$(openssl rand -base64 32)
-
-LAN_PROXY=socks5://192.168.1.6:1089
-LOCAL_PROXY=socks5://127.0.0.1:1089
+source .local_env
 
 ENV="NODE_ENV=development\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
@@ -14,18 +9,22 @@ NEXTAUTH_URL=http://localhost:3000\n\
 NEXT_PUBLIC_BACKEND_URL=http://localhost:3000\n\
 OPENAI_API_KEY=$OPENAI_API_KEY\n\
 DATABASE_URL=file:../db/db.sqlite\n\
-HTTP_PROXY=$LAN_PROXY\n\
-HTTPS_PROXY=$LAN_PROXY\n"
+HTTP_PROXY=$LOCAL_PROXY\n\
+HTTPS_PROXY=$LOCAL_PROXY\n\
+NEXT_PUBLIC_WEB_SEARCH_ENABLED=true\n\
+SERP_API_KEY=$SERP_API_KEY\n"
+
+echo $ENV
 
 cd next
 printf $ENV > .env
 printf $ENV > .env.docker
 
+cd ..
 docker stop agentgpt
 docker rm agentgpt
 
-source .env.docker
-docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
+docker-compose -f ./local-docker-compose.yml up -d --remove-orphans
 
 cd ..
 sleep 10
