9ade3f81491bec09d2d0412c545321fc17b497bb
yhyu13 <yohan680919@gmail.com>
2023-05-17 19:41:52 +0800

    Update local script

--- local_setup.sh
+++ local_setup.sh
@@ -3,17 +3,17 @@ cd "$(dirname "$0")" || exit
 
 source .local_env
 
-LAN_PROXY=http://192.168.1.6:8889
-LOCAL_PROXY=http://127.0.0.1:8889
-
 ENV="NODE_ENV=development\n\
 NEXTAUTH_SECRET=$NEXTAUTH_SECRET\n\
 NEXTAUTH_URL=http://localhost:3000\n\
 NEXT_PUBLIC_BACKEND_URL=http://localhost:3000\n\
 OPENAI_API_KEY=$OPENAI_API_KEY\n\
+OPENAI_API_BASE=$OPENAI_API_BASE\n\
 DATABASE_URL=file:../db/db.sqlite\n\
 NEXT_PUBLIC_WEB_SEARCH_ENABLED=true\n\
-SERP_API_KEY=$SERP_API_KEY\n"
+SERP_API_KEY=$SERP_API_KEY\n\
+HTTP_PROXY=$HTTP_PROXY\n\
+HTTPS_PROXY=$HTTPS_PROXY\n"
 
 echo $ENV
 
@@ -21,11 +21,25 @@ cd next
 printf $ENV > .env
 printf $ENV > .env.docker
 
-cd ..
 docker stop agentgpt
 docker rm agentgpt
 
-docker-compose -f ./local-docker-compose.yml up --build -d --remove-orphans
+if [ "$1" = "--docker" ]; then
+  source .env.docker
+  docker build --build-arg NODE_ENV=$NODE_ENV -t agentgpt .
+  docker run -d --name agentgpt -p 3000:3000 -v $(pwd)/db:/app/db agentgpt
 
-sleep 10
-xdg-open http://localhost:3000
+  sleep 10
+  xdg-open http://localhost:3000
+elif [ "$1" = "--docker-compose" ]; then
+  cd ..
+  docker-compose up -d --remove-orphans
+
+  sleep 10
+  xdg-open http://localhost:3000
+else
+  ./prisma/useSqlite.sh
+  npm install
+  prisma db push
+  npm run dev
+fi
